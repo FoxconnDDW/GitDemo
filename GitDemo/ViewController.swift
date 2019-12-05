bcac95617ae0bd9078abdc80b611bae11a57ab00
FoxconnDDW <18662395023@163.com>
2019-12-05 13:29:42 +0800

    Initial Commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by foxconn on 2019/12/5.
+//  Copyright © 2019 foxconn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
