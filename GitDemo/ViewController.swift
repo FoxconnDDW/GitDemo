3daf9eaae059106c5ec08f8f531fb4d6ec3d1c7e
FoxconnDDW <18662395023@163.com>
2019-12-05 15:19:20 +0800

    开始

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("haha")
         print("ccc")
+        print("shangchuan")
         // Do any additional setup after loading the view.
     }
 
